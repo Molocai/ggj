83076a711aa2489634f94bf838f3d453d5c43903
Titouan <molocai@molocai.com>
2017-01-21 01:39:04 +0100

    Stuff

---
+++ Assets/Ship.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ship : MonoBehaviour
+{
+    void Update()
+    {
+        Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
+        newPos.y = SeaCalculator.GetWorldHeight(transform.position);
+
+        if (Input.GetKey(KeyCode.Z))
+            newPos.x += Time.deltaTime * 3f;
+
+        if (Input.GetKey(KeyCode.S))
+            newPos.x -= Time.deltaTime * 3f;
+
+        if (Input.GetKey(KeyCode.Q))
+            newPos.z += Time.deltaTime * 3f;
+
+        if (Input.GetKey(KeyCode.D))
+            newPos.z -= Time.deltaTime * 3f;
+
+        transform.position = newPos;
+    }
+}
---
+++ Assets/Wave.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Wave : MonoBehaviour
+{
+    private Vector3[] baseHeight;
+
+    void Update()
+    {
+        Mesh mesh = GetComponent<MeshFilter>().mesh;
+
+        if (baseHeight == null)
+            baseHeight = mesh.vertices;
+
+        Vector3[] vertices = new Vector3[baseHeight.Length];
+        for (int i = 0; i < vertices.Length; i++)
+        {
+            vertices[i] = baseHeight[i];
+            vertices[i].y = SeaCalculator.GetWorldHeight(transform.TransformPoint(vertices[i]));
+        }
+
+        mesh.vertices = vertices;
+        mesh.RecalculateNormals();
+    }
+}
