9619856d4eafd3feebe3ff89127db7505be4f17c
Titouan <molocai@molocai.com>
2017-01-21 02:51:57 +0100

    Ship de ouf

--- Assets/Ship.cs
+++ Assets/Ship.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class Ship : MonoBehaviour
 {
+    public Transform Nose;
+    public Transform Tail;
+
     void Update()
     {
         Vector3 newPos = new Vector3(transform.position.x, transform.position.y, transform.position.z);
@@ -22,5 +25,22 @@ public class Ship : MonoBehaviour
             newPos.z -= Time.deltaTime * 3f;
 
         transform.position = newPos;
+
+        Vector3 noseNewPos = new Vector3(Nose.position.x, Nose.position.y, Nose.position.z);
+        Vector3 tailNewPos = new Vector3(Tail.position.x, Tail.position.y, Tail.position.z);
+
+        noseNewPos.y = SeaCalculator.GetWorldHeight(Nose.position);
+        tailNewPos.y = SeaCalculator.GetWorldHeight(Tail.position);  
+
+        Vector3 magicVector = noseNewPos - tailNewPos;
+
+        Debug.DrawLine(transform.position, transform.position + magicVector);
+
+        //Quaternion pouet = Quaternion.FromToRotation(transform.up, magicVector);
+        //Quaternion pouet = Quaternion.LookRotation(magicVector, transform.up);
+
+        //transform.rotation = pouet;
+
+        transform.LookAt(transform.position + magicVector);
     }
 }
