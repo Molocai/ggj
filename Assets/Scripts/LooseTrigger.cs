1b9ff57f047c1236f8da2f6b9ee537c52b59b339
RIVOIRE Francois <RIVOIRE.francois@formation.enjmin.fr>
2017-01-22 13:41:55 +0100

    Loose trigger

---
+++ Assets/Scripts/LooseTrigger.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+[RequireComponent (typeof(Rigidbody))]
+public class LooseTrigger : MonoBehaviour {
+
+    private Rigidbody _rigidbody;
+
+    void OnTriggerEnter(Collider other)
+    {
+        if(other.tag == "Player")
+        {
+            Debug.Log("YOU LOOSE YOU LOOSER !");
+        }
+    }
+
+	// Use this for initialization
+	void Start () {
+        _rigidbody = GetComponent<Rigidbody>();
+        _rigidbody.isKinematic = true;
+        _rigidbody.useGravity = false;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
